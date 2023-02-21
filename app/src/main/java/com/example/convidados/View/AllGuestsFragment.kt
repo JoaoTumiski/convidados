6ffebce1035558dc7a2c185c956a8966b9ca35a7
JoãoTumiski <joaotumiski@gmail.com>
2023-02-20 22:11:24 -0300

    Banco de dados, listagem

--- app/src/main/java/com/example/convidados/View/AllGuestsFragment.kt
+++ app/src/main/java/com/example/convidados/View/AllGuestsFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import com.example.convidados.ViewModel.AllGuestsViewModel
@@ -14,25 +13,24 @@ class AllGuestsFragment : Fragment() {
 
     private var _binding: FragmentAllGuestsBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
+
+    private lateinit var  viewModel: AllGuestsViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val allGuestsViewModel =
-            ViewModelProvider(this).get(AllGuestsViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(AllGuestsViewModel::class.java)
 
         _binding = FragmentAllGuestsBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        allGuestsViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        viewModel.getAll()
+
+        observe()
+
         return root
     }
 
@@ -40,4 +38,10 @@ class AllGuestsFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    private fun observe(){
+        viewModel.guests.observe(viewLifecycleOwner){
+        }
+    }
+
 }
